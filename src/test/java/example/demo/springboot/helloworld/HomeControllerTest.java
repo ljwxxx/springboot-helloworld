af098cb86402944886658dc8a1758873cf846d1c
ljwxxx <38178341+ljwxxx@users.noreply.github.com>
2019-05-16 16:14:40 +0800

    Update HomeControllerTest.java

--- src/test/java/example/demo/springboot/helloworld/HomeControllerTest.java
+++ src/test/java/example/demo/springboot/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello World!");
+            .assertResponseBody("Hello World! 20190516");
     }
 
     private HelloWorldResponse getRequest(String uri) {

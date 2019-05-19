2ce34fdf167a791d5498ae1159bffbcf040e7ced
ljwxxx <38178341+ljwxxx@users.noreply.github.com>
2019-05-19 20:53:46 +0800

    Update HomeController.java

--- src/main/java/example/demo/springboot/helloworld/controller/HomeController.java
+++ src/main/java/example/demo/springboot/helloworld/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
     @RequestMapping("/")
     String home() {
         //from Test CI/CD
-        String msg = "Hello World! 20190519";
+        String msg = "Hello World! 20190520";
         System.out.println(msg);
         return msg;
     }
